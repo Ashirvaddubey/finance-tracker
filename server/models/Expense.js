2e83327d011fc5299644f189b74101f6ca35e736
Ashirvad dubey <114186314+Ashirvaddubey@users.noreply.github.com>
2025-06-25 22:36:04 +0530

    Update Expense.js

--- server/models/Expense.js
+++ server/models/Expense.js
@@ -1,5 +1,4 @@
 import mongoose from 'mongoose';
-
 const expenseSchema = new mongoose.Schema({
   user: {
     type: mongoose.Schema.Types.ObjectId,
@@ -50,9 +49,6 @@ const expenseSchema = new mongoose.Schema({
 }, {
   timestamps: true
 });
-
-// Index for better query performance
 expenseSchema.index({ user: 1, date: -1 });
 expenseSchema.index({ user: 1, category: 1 });
-
 export default mongoose.model('Expense', expenseSchema);
